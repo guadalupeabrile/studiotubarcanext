75b7d1db3dc729482e870ba43d435e02a71f7b97
guadalupeabrile <guadalupeabrile@gmail.com>
2023-12-06 18:00:21 +1000

    igiardini video controls

--- components/project-video.tsx
+++ components/project-video.tsx
@@ -6,7 +6,7 @@ const ProjectVideo = () => {
         <section className='min-h-screen w-full'>
             <div className="">
                 <div className="">
-                    <video autoPlay loop muted className="w-full h-screen object-cover">
+                    <video autoPlay controls loop muted className="w-full h-screen object-cover">
                         <source
                             src='/assets/blog/projects-videos/igiardini-video.mp4'
                             type="video/mp4"
@@ -15,7 +15,7 @@ const ProjectVideo = () => {
 
                 </div>
                 <div className='md:hidden'>
-                    <video autoPlay muted loop playsInline className='absolute h-full w-full flex items-center justify-center -bottom-0 '>
+                    <video autoPlay muted controls loop playsInline className='absolute h-full w-full flex items-center justify-center -bottom-0 '>
                         <source src='/assets/blog/projects-videos/igiardini-video-mobile.mp4' type="video/mp4" />
                     </video>
                 </div>
