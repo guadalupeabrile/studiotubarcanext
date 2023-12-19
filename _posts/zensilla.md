87911c23974e29298018ab872698ef0aebc0ba5a
guadalupeabrile <guadalupeabrile@gmail.com>
2023-12-20 08:05:05 +1000

    gif animation logos

--- _posts/zensilla.md
+++ _posts/zensilla.md
@@ -3,7 +3,7 @@ title: 'Zensilla'
 excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
 coverImage: '/assets/blog/projects-preview/zensilla-preview.png'
 nameImage: '/assets/blog/projects-name/zensilla-name.png'
-nameVideo: '/assets/blog/project-animation/igiardini-logo.mp4'
+nameVideo: '/assets/blog/project-animation/zensilla-logo.gif'
 date: '2020-03-16T05:35:07.322Z'
 author:
   name: Tim Neutkens
--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -16,7 +16,7 @@ type Props = {
 const CoverImage = ({ title, src, slug, nameVideo }: Props) => {
   const image = (
     <Image
-      src={src}
+      src='/assets/blog/project-animation/zensilla-logo.gif'
       alt={`Cover Image for ${title}`}
       className=" w-full object-cover h-auto group-hover:shadow-lg group-hover:opacity-0  transition-shadow duration-200"
       width={1300}
@@ -26,20 +26,20 @@ const CoverImage = ({ title, src, slug, nameVideo }: Props) => {
 
   //Mouseover effect for md and lg screens
 
-  const videoId = title;
+  // const videoId = title;
 
-  useEffect(() => {
-    const anim = document.getElementById(videoId) as HTMLVideoElement;
+  // useEffect(() => {
+  //   const anim = document.getElementById(videoId) as HTMLVideoElement;
 
-    anim.addEventListener("mouseover", function () {
-      anim.play();
-    });
+  //   anim.addEventListener("mouseover", function () {
+  //     anim.play();
+  //   });
 
-    anim.addEventListener("mouseout", function () {
-      anim.pause();
-      anim.currentTime = 0; // Reiniciar el video al principio al salir del área del video
-    });
-  }, [])
+  //   anim.addEventListener("mouseout", function () {
+  //     anim.pause();
+  //     anim.currentTime = 0; // Reiniciar el video al principio al salir del área del video
+  //   });
+  // }, [])
 
   //InView autoPlay for mobile 
 
@@ -55,20 +55,37 @@ const CoverImage = ({ title, src, slug, nameVideo }: Props) => {
           <div className='relative'>
             <Link as={`/projects/${slug}`} href={`/projects/${slug}`} aria-label={title}>
 
-              {image}
+              {/* <Image
+                src={src}
+                alt='studiotubarca Home'
+                width={1300}
+                height={630}
+                className='group-hover:shadow-lg group-hover:opacity-0  transition-shadow duration-200' /> */}
 
-              <div className='hidden md:block'>
+
+              <Image
+                src='/assets/blog/project-animation/zensilla-logo.gif'
+                alt='studiotubarca Home'
+                width={1300}
+                height={630}
+                className='shadow-lg   h-full w-full flex items-center justify-center ' />
+
+
+              {/* {image} */}
+
+
+              {/* <div className='hidden md:block'>
                 <video id={title} preload='none' muted loop className='shadow md:opacity-0 md:group-hover:opacity-90  absolute h-full w-full flex items-center justify-center -bottom-0 '>
                   <source src={nameVideo} type="video/mp4" />
                 </video>
-              </div>
+              </div> */}
 
             </Link>
-            <div ref={myRef} className={`${inView ? 'block' : 'hideen'}   md:hidden`}>
+            {/* <div ref={myRef} className={`${inView ? 'block' : 'hideen'}   md:hidden`}>
               <video id={title} autoPlay muted loop playsInline className='absolute h-full w-full flex items-center justify-center -bottom-0 '>
                 <source src={nameVideo} type="video/mp4" />
               </video>
-            </div>
+            </div> */}
 
           </div>
         </div>
