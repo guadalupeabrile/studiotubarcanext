97257a758bad84eed2bdedd847a3379257ee70e0
guadalupeabrile <guadalupeabrile@gmail.com>
2023-12-29 14:59:13 +1000

    content change manifiesto

--- pages/manifiesto/index.tsx
+++ pages/manifiesto/index.tsx
@@ -14,7 +14,7 @@ export default function Manifiesto() {
             <div className="relative bg-manifiesto-flor h-screen w-screen flex flex-col items-center justify-center">
                 <div className="p-5 md:p-20 text-center font-light md:text-3xl">
                     <p className="leading-loose">Arquitect degree, artist, carpenter, cooker and explorer by trade.</p>
-                    <p className="leading-loose ">I seek fertile experiences that nourish my daily endeavors. I am a manifestor of ideas that bring hoy to people's lives. I focus on providing solutions to contemporary problems by integrating essential variables for personal growth.</p>
+                    <p className="leading-loose ">I seek fertile experiences that nourish my daily endeavors. I am a manifestor of ideas that <span className="hidden md:inline"><br /></span> bring hoy to people's lives. I focus on providing solutions to contemporary problems <span className="hidden md:inline"><br /></span> by integrating essential variables for personal growth.</p>
                     <p className="leading-loose">In this way, I facilitate proposals that enchance the quality of experieces</p>
                 </div>
                 <div>
@@ -39,8 +39,8 @@ export default function Manifiesto() {
                     objectFit="cover" />
             </div>
 
-            <div className="relative bg-manifiesto-flor h-screen w-screen flex flex-col items-center justify-center">
-                <div className="p-5 md:p-20 text-center font-light md:text-3xl">
+            <div className="relative bg-custom-background h-screen w-screen flex flex-col items-center justify-center">
+                <div className="bg-manifiesto-perro p-5 md:p-36 text-center font-light md:text-3xl">
                     <p className="leading-loose">I follow the path guided by my intuition and my purpose, centered on emboding insights from the opportunities that come my way. I aim to share teachings and knowledge as my mentors do with me, in a quest for mutual collaboration. I have crafted a curriculum based on study, travels, experimentation, and experiences that provide me with the tools on a daily basis. </p>
                     <p className="leading-loose">Santiago Ruiz.</p>
                 </div>
