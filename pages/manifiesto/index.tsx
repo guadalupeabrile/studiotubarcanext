26b869b298bea0c27c264fd87516b69100402e86
guadalupeabrile <guadalupeabrile@gmail.com>
2024-01-10 15:09:35 +1000

    br en texto manifiesto pc

--- pages/manifiesto/index.tsx
+++ pages/manifiesto/index.tsx
@@ -13,9 +13,11 @@ export default function Manifiesto() {
 
             <div className="relative bg-manifiesto-flor h-screen w-screen flex flex-col items-center justify-center">
                 <div className="p-5 md:p-20 text-center font-light text-xs md:text-3xl">
-                    <p className="leading-loose">De formación Arquitecto, Cocinero, de oficio Artista, Carpintero y Explorador.</p>
-                    <p className="leading-loose ">Busco experiencias fértiles que alimenten mi quehacer diario. Soy un manifestador de <span className="hidden md:inline"><br /></span>propuestas que alegran la vida de las personas. Me enfoco en dar soluciones a problemas <span className="hidden md:inline"><br /></span>contemporáneos integrando variables indispensables para el crecimiento personal.</p>
-                    <p className="leading-loose">Asi facilitar propuestas que potencien la calidad de las experiencias</p>
+                    <p className="leading-loose">Sobre mí. De formación Arquitecto, Cocinero,<span className="inline md:hidden"><br /></span> de oficio Artista, Carpintero y Explorador.</p>
+                    <p className="leading-relaxed">Busco experiencias fértiles que alimenten mi quehacer diario. </p>
+                    <p className="leading-relaxed">Soy un manifestador de propuestas que alegran la vida de las personas.</p>
+                    <p className="leading-relaxed">Me enfoco en dar soluciones a problemas contemporáneos  <span className="hidden md:inline"><br /></span>integrando variables indispensables para el crecimiento personal.</p>
+                    <p className="leading-loose">Así facilitar propuestas que potencien la calidad de las experiencias.</p>
                 </div>
                 <div>
                     <Link as={`/`} href={`/`} aria-label='studiotubarca home'>
@@ -41,9 +43,10 @@ export default function Manifiesto() {
 
             <div className="relative bg-custom-background h-screen w-screen flex flex-col items-center justify-center">
                 <div className="bg-manifiesto-perro p-5 md:p-32 text-center font-light text-xs md:text-3xl">
-                    <p className="leading-loose">Sigo el camino guiado por mi intuición y mi propósito centrado en tomar el máximo aprendizaje <span className="hidden md:inline"><br /></span>de las propuestas que se me presentan. Busco compartir enseñanzas y conocimientos cómo <span className="hidden md:inline"><br /></span>mis maestros lo hacen conmigo, en una búsqueda de colaboración mutua.</p>
-                    <p className="leading-loose">Generé un Curriculum basado en el estudio, los viajes, la experimentación y las experiencias <span className="hidden md:inline"><br /></span>que me brindan herramientas a diario.</p>
-                    <p className="leading-loose pt-5">Santiago Ruiz.</p>
+                    <p className="leading-relaxed">Sigo el camino guiado por mi intuición y mi propósito centrado en tomar <span className="hidden md:inline"><br /></span>el máximo aprendizaje de las propuestas que se me presentan.</p>
+                    <p className="leading-relaxed">Busco compartir enseñanzas y conocimientos cómo <span className="hidden md:inline"><br /></span>mis maestros lo hacen conmigo, en una búsqueda de colaboración mutua.</p>
+                    <p className="leading-relaxed pt-5">Generé un Curriculum basado en el estudio, los viajes,  <span className="hidden md:inline"><br /></span>la experimentación y las experiencias que me brindan herramientas a diario.</p>
+                    <p className="leading-loodr pt-10">Santiago Ruiz.</p>
                 </div>
                 <div className="flex flex-col md:flex-row items-center space-y-5 md:space-x-5 pb-5">
                     <Link as={`/`} href={`/`} aria-label='studiotubarca home'>
