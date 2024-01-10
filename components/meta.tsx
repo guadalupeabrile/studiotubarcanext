a75d04c4a4ea9a153c28302dc742df3bc82d8e44
guadalupeabrile <guadalupeabrile@gmail.com>
2024-01-10 16:40:13 +1000

    meta data

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,14 +3,12 @@ import Head from 'next/head'
 const Meta = () => {
   return (
     <Head>
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" content="#000" />
+      <link rel="icon" href="/favicon/favicon.ico" />
       <meta
         name="studiotubarca"
         content={`Creative. Arquitecture. Carpintry.`}
       />
-      <meta property="og:image" content='/assets/blog/manifiesto/arte-di-construire.png' />
+      <meta property="og:image" content='/favicon/opengraph-image.png' />
     </Head>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Meta from '../components/meta'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Meta />
+      </Head>
       <body className='font-pro'>
         <Main />
         <NextScript />
