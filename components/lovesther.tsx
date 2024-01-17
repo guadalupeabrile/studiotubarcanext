8b19426c3ac74fcc7120c0d666153edb6b8d9954
guadalupeabrile <guadalupeabrile@gmail.com>
2024-01-18 09:25:14 +1000

    lovesther content added

--- components/lovesther.tsx
+++ components/lovesther.tsx
@@ -5,14 +5,8 @@ import { fadeIn } from '../utils/motionTransitions'
 
 import url1 from '../public/assets/blog/love-img/love-langosta.png';
 import url2 from '../public/assets/blog/love-img/love-eyes.png'
-import url3 from '../public/assets/blog/mesatelite-img/mesat-1.jpg'
-import url4 from '../public/assets/blog/mesatelite-img/mesat-diag.png'
-import url5 from '../public/assets/blog/mesatelite-img/mesat-2.jpg'
-import url6 from '../public/assets/blog/mesatelite-img/mesat-3.jpg'
-import url7 from '../public/assets/blog/mesatelite-img/mesat-4.jpg'
-import url8 from '../public/assets/blog/mesatelite-img/mesat-5.png'
-import url9 from '../public/assets/blog/mesatelite-img/mesat-6.jpg'
-import flor from '../public/assets/blog/footer/flor-manifiesto.png';
+import url3 from '../public/assets/blog/love-img/love-lows.png';
+
 
 
 
@@ -23,7 +17,7 @@ export default function Lovesther() {
         <>
 
 
-            <div className="relative h-screen w-screen box-border bg-love-background bg-custom-background">
+            <div className="relative h-auto w-screen box-border bg-love-background bg-custom-background pb-10">
                 <motion.div
                     className="md:hidden absolute -top-48"
                     variants={fadeIn('right', 0.5)} initial="hidden" animate="show" exit='hidden'
@@ -61,8 +55,31 @@ export default function Lovesther() {
                 >
                     <Image
                         src={url2}
-                        alt={`Diagramas Mesatelites`}
+                        alt={`Ojos Lovesther`}
                         className='w-3/5'
+                        style={{ objectFit: "cover" }}
+
+                    />
+                </motion.div>
+                <motion.div
+                    className="text-center md:text-left text-sm md:text-xl bg-yellow-100/50 m-10 mt-16 md:mt-32"
+                    variants={fadeIn('right', 0.5)} initial="hidden" animate="show" exit='hidden'
+                >
+                    <p>ESTHER es una langosta que sale del OCÉANO al notar que se ocupa de limpiar su casa cada vez más seguido. Encuentra elementos que no pertenecen a ese medio.</p>
+                    <p>Al salir del AGUA observa que esos objetos se utilizan normalmente por los humanos. Se comunica con personas que disfrutan en la playa y les comunica un claro mensaje:</p>
+                    <p>"SOMOS UNO, LA TIERRA, EL AGUA, EL AIRE, NOSOTROS. LIMPIEMOS Y CUIDEMOS NUESTRA CASA COMO A NOSOTROS.</p>
+                    <p>A partir de este intercambio, generamos una marca universal para manifestar un mensaje. Sabemos que así llegará a muchas personas. ESTHER comunica a través del ejemplo, de la CONCIENCIA.</p>
+                    <p>Generamos un equipo de trabajo en dónde cada uno entrega lo mejor de sí, porque sabe que forma parte de un todo más grande, en un propósito común. Transmitimos mediante el lenguaje del AMOR.</p>
+                    <p>A partir de lo que usamos normalmente, y queda en desuso. Gracias a una selección minuciosa, mediante el diseño y el esfuerzo de los trabajadores locales, confeccionamos ropa durable, de gran calidad, única, reciclada, artesanal.</p>
+                </motion.div>
+                <motion.div
+                    className="relative flex justify-center "
+                    variants={fadeIn('down', 0.7)} initial="hidden" animate="show" exit='hidden'
+                >
+                    <Image
+                        src={url3}
+                        alt={`Ojos Lovesther`}
+                        className='w-2/5 pt-10'
                         style={{ objectFit: "cover" }}
 
                     />
